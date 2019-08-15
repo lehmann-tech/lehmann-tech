642ca37bb1df5c576e5b3b40d7c5e222ea9bdadc
Martin Lehmann <martin@folio.no>
2019-08-15 16:59:51 +0200

    blah

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 # Setup
+provider "google" {
+  project = "lehmann-tech"
+  region  = "europe-west1"
+  zone    = "europe-west1-b"
+}
+
 provider "google-beta" {
   project = "lehmann-tech"
   region  = "europe-west1"
@@ -28,51 +34,94 @@ provider "google-beta" {
 #   }
 # }
 
-# # dev environment
-# resource "google_compute_network" "dev_vpc_network" {
-#   name                    = "dev"
-#   auto_create_subnetworks = "true"
-# }
+# dev environment
 
-# resource "google_container_cluster" "dev_container_cluster" {
-#   name     = "dev"
-#   location = "europe-west1"
+# Note: KeyRings cannot be deleted from Google Cloud Platform.
+# Destroying a Terraform-managed KeyRing will remove it from state
+# but will not delete the resource on the server.
+# https://www.terraform.io/docs/providers/google/r/kms_key_ring.html
+resource "google_kms_key_ring" "kms_keyring_dev" {
+  name     = "dev"
+  location = "europe-west1"
+}
 
-#   network = "${google_compute_network.dev_vpc_network.self_link}"
+resource "google_kms_crypto_key" "kms_crypto_key_etcd_database_password_dev" {
+  name = "etcd-database-password"
+  key_ring = "${google_kms_key_ring.kms_keyring_dev.self_link}"
+}
 
-#   # We can't create a cluster with no node pool defined, but we want to only use
-#   # separately managed node pools. So we create the smallest possible default
-#   # node pool and immediately delete it.
-#   remove_default_node_pool = true
-#   initial_node_count = 1
+resource "google_compute_network" "dev_vpc_network" {
+  name                    = "dev"
+  auto_create_subnetworks = "true"
+}
 
-#   master_auth {
-#     username = ""
-#     password = ""
+# staging environment
 
-#     client_certificate_config {
-#       issue_client_certificate = false
-#     }
-#   }
-# }
+# Note: KeyRings cannot be deleted from Google Cloud Platform.
+# Destroying a Terraform-managed KeyRing will remove it from state
+# but will not delete the resource on the server.
+# https://www.terraform.io/docs/providers/google/r/kms_key_ring.html
+resource "google_kms_key_ring" "kms_keyring_staging" {
+  name     = "staging"
+  location = "europe-west1"
+}
 
-# resource "google_container_node_pool" "dev_container_node_pool" {
-#   name       = "dev-node-pool"
-#   location   = "europe-west1"
-#   cluster    = "${google_container_cluster.dev_container_cluster.name}"
-#   node_count = 2
+resource "google_kms_crypto_key" "kms_crypto_key_etcd_database_password_staging" {
+  name = "etcd-database-password"
+  key_ring = "${google_kms_key_ring.kms_keyring_staging.self_link}"
+}
 
-#   node_config {
-#     machine_type = "n1-standard-1"
+resource "google_compute_network" "staging_vpc_network" {
+  name                    = "staging"
+  auto_create_subnetworks = "true"
+}
 
-#     metadata = {
-#       disable-legacy-endpoints = "true"
-#     }
+resource "google_container_cluster" "staging_container_cluster" {
+  provider = "google-beta"
 
-#     oauth_scopes = [
-#       "https://www.googleapis.com/auth/logging.write",
-#       "https://www.googleapis.com/auth/monitoring",
-#       "https://www.googleapis.com/auth/cloud-platform"
-#     ]
-#   }
-# }
+  name     = "staging"
+  location = "europe-west1"
+
+  network = "${google_compute_network.staging_vpc_network.self_link}"
+
+  database_encryption {
+    state = "ENCRYPTED"
+    key_name = "${google_kms_crypto_key.kms_crypto_key_etcd_database_password_staging.self_link}"
+  }
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count = 1
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+resource "google_container_node_pool" "staging_container_node_pool" {
+  name       = "staging-node-pool"
+  location   = "europe-west1"
+  cluster    = "${google_container_cluster.staging_container_cluster.name}"
+  node_count = 2
+
+  node_config {
+    machine_type = "n1-standard-1"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
