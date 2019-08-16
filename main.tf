fe77e930a1b111026f821ac3e07a8bd79dd64c10
Martin Lehmann <martin@folio.no>
2019-08-16 23:14:45 +0200

    add unapplied dev cluster, waiting for quota increase

--- main.tf
+++ main.tf
@@ -35,16 +35,35 @@ resource "google_dns_record_set" "dns_record_set_lehmann_tech" {
   ]
 }
 
+resource "google_compute_global_address" "ip_address_dev" {
+  name = "dev"
+}
+
+resource "google_compute_global_address" "ip_address_staging" {
+  name = "staging"
+}
+
 resource "google_dns_managed_zone" "dns_zone_unwanted_fun" {
   name = "unwanted-fun"
   dns_name = "unwanted.fun."
 }
 
-resource "google_dns_record_set" "dns_record_set_unwanted_fun" {
+resource "google_dns_record_set" "dns_record_set_unwanted_fun_dev" {
+  name = "dev.${google_dns_managed_zone.dns_zone_unwanted_fun.dns_name}"
+  managed_zone = "${google_dns_managed_zone.dns_zone_unwanted_fun.name}"
+  type = "A"
+  ttl = 300 # seconds = 5 minutes
+
+  rrdatas = [
+    "${google_compute_global_address.ip_address_dev.address}"
+  ]
+}
+
+resource "google_dns_record_set" "dns_record_set_unwanted_fun_staging" {
   name = "staging.${google_dns_managed_zone.dns_zone_unwanted_fun.dns_name}"
   managed_zone = "${google_dns_managed_zone.dns_zone_unwanted_fun.name}"
   type = "A"
-  ttl = 300 # seconds
+  ttl = 300 # seconds = 5 minutes
 
   rrdatas = [
     "${google_compute_global_address.ip_address_staging.address}"
@@ -85,16 +104,71 @@ resource "google_kms_key_ring" "kms_keyring_dev" {
   location = "europe-west1"
 }
 
-# Waiting for a scheduled deletion
+resource "google_kms_crypto_key" "kms_crypto_key_etcd_database_encryption_password_dev" {
+  name     = "etcd-database-encryption-password"
+  key_ring = "${google_kms_key_ring.kms_keyring_dev.self_link}"
+}
 
-# resource "google_kms_crypto_key" "kms_crypto_key_etcd_database_password_dev" {
-#   name     = "etcd-database-password"
-#   key_ring = "${google_kms_key_ring.kms_keyring_dev.self_link}"
-# }
-
-resource "google_compute_network" "dev_vpc_network" {
+resource "google_compute_network" "vpc_network_dev" {
   name                    = "dev"
   auto_create_subnetworks = "true"
+}
+
+data "google_container_engine_versions" "container_engine_versions_dev" {
+  version_prefix = "1.13."
+}
+
+resource "google_container_cluster" "dev_container_cluster" {
+  provider = "google-beta"
+
+  name     = "dev"
+  location = "europe-west1"
+  min_master_version = "${data.google_container_engine_versions.container_engine_versions_dev.latest_master_version}"
+
+  network = "${google_compute_network.vpc_network_dev.self_link}"
+
+  database_encryption {
+    state    = "ENCRYPTED"
+    key_name = "${google_kms_crypto_key.kms_crypto_key_etcd_database_encryption_password_dev.self_link}"
+  }
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+resource "google_container_node_pool" "dev_container_node_pool" {
+  name       = "dev-node-pool"
+  location   = "europe-west1"
+  cluster    = "${google_container_cluster.dev_container_cluster.name}"
+  node_count = 2
+
+  version = "${data.google_container_engine_versions.container_engine_versions_dev.latest_master_version}"
+
+  node_config {
+    machine_type = "n1-standard-1"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
 }
 
 # staging environment
@@ -119,7 +193,6 @@ resource "google_compute_network" "staging_vpc_network" {
 }
 
 data "google_container_engine_versions" "container_engine_versions_staging" {
-  location       = "us-central1-b"
   version_prefix = "1.13."
 }
 
@@ -174,8 +247,4 @@ resource "google_container_node_pool" "staging_container_node_pool" {
       "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
-}
-
-resource "google_compute_global_address" "ip_address_staging" {
-  name = "staging"
 }
