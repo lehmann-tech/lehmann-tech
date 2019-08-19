a1cc5b7efe8a81d25a8153b45a3446b4ba04c847
Martin Lehmann <martin@folio.no>
2019-08-19 12:58:38 +0200

    extract environment to module

---
+++ terraform/environments/environment.tf
@@ -0,0 +1,79 @@
+variable "cluster_name" {
+  type = string
+}
+
+# Note: KeyRings cannot be deleted from Google Cloud Platform.
+# Destroying a Terraform-managed KeyRing will remove it from state
+# but will not delete the resource on the server.
+# https://www.terraform.io/docs/providers/google/r/kms_key_ring.html
+resource "google_kms_key_ring" "kms_keyring" {
+  name     = var.cluster_name
+  location = "europe-west1"
+}
+
+resource "google_kms_crypto_key" "kms_crypto_key_etcd_database_encryption_password" {
+  name     = "etcd-database-encryption-password"
+  key_ring = google_kms_key_ring.kms_keyring.self_link
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = var.cluster_name
+  auto_create_subnetworks = "true"
+}
+
+data "google_container_engine_versions" "container_engine_versions" {
+  version_prefix = "1.13."
+}
+
+resource "google_container_cluster" "container_cluster" {
+  provider = "google-beta"
+
+  name     = var.cluster_name
+  location = "europe-west1"
+  min_master_version = data.google_container_engine_versions.container_engine_versions.latest_master_version
+
+  network = google_compute_network.vpc_network.self_link
+
+  database_encryption {
+    state    = "ENCRYPTED"
+    key_name = google_kms_crypto_key.kms_crypto_key_etcd_database_encryption_password.self_link
+  }
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+resource "google_container_node_pool" "container_node_pool" {
+  name       = "${var.cluster_name}-node-pool"
+  location   = "europe-west1"
+  cluster    = google_container_cluster.container_cluster.name
+  node_count = 2
+
+  version = data.google_container_engine_versions.container_engine_versions.latest_master_version
+
+  node_config {
+    machine_type = "n1-standard-1"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -19,22 +19,6 @@ resource "google_compute_ssl_policy" "ssl_policy" {
   min_tls_version = "TLS_1_2"
 }
 
-resource "google_dns_managed_zone" "dns_zone_lehmann_tech" {
-  name = "lehmann-tech"
-  dns_name = "lehmann.tech."
-}
-
-resource "google_dns_record_set" "dns_record_set_lehmann_tech" {
-  name = "staging.${google_dns_managed_zone.dns_zone_lehmann_tech.dns_name}"
-  managed_zone = "${google_dns_managed_zone.dns_zone_lehmann_tech.name}"
-  type = "A"
-  ttl = 300 # seconds
-
-  rrdatas = [
-    "${google_compute_global_address.ip_address_staging.address}"
-  ]
-}
-
 resource "google_compute_global_address" "ip_address_dev" {
   name = "dev"
 }
@@ -72,156 +56,16 @@ resource "google_dns_record_set" "dns_record_set_unwanted_fun_staging" {
 
 # dev environment
 
-# Note: KeyRings cannot be deleted from Google Cloud Platform.
-# Destroying a Terraform-managed KeyRing will remove it from state
-# but will not delete the resource on the server.
-# https://www.terraform.io/docs/providers/google/r/kms_key_ring.html
-resource "google_kms_key_ring" "kms_keyring_dev" {
-  name     = "dev"
-  location = "europe-west1"
-}
+module "dev_environment" {
+  source = "./environments"
 
-resource "google_kms_crypto_key" "kms_crypto_key_etcd_database_encryption_password_dev" {
-  name     = "etcd-database-encryption-password"
-  key_ring = "${google_kms_key_ring.kms_keyring_dev.self_link}"
-}
-
-resource "google_compute_network" "vpc_network_dev" {
-  name                    = "dev"
-  auto_create_subnetworks = "true"
-}
-
-data "google_container_engine_versions" "container_engine_versions_dev" {
-  version_prefix = "1.13."
-}
-
-resource "google_container_cluster" "dev_container_cluster" {
-  provider = "google-beta"
-
-  name     = "dev"
-  location = "europe-west1"
-  min_master_version = "${data.google_container_engine_versions.container_engine_versions_dev.latest_master_version}"
-
-  network = "${google_compute_network.vpc_network_dev.self_link}"
-
-  database_encryption {
-    state    = "ENCRYPTED"
-    key_name = "${google_kms_crypto_key.kms_crypto_key_etcd_database_encryption_password_dev.self_link}"
-  }
-
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
-  remove_default_node_pool = true
-  initial_node_count       = 1
-
-  master_auth {
-    username = ""
-    password = ""
-
-    client_certificate_config {
-      issue_client_certificate = false
-    }
-  }
-}
-
-resource "google_container_node_pool" "dev_container_node_pool" {
-  name       = "dev-node-pool"
-  location   = "europe-west1"
-  cluster    = "${google_container_cluster.dev_container_cluster.name}"
-  node_count = 2
-
-  version = "${data.google_container_engine_versions.container_engine_versions_dev.latest_master_version}"
-
-  node_config {
-    machine_type = "n1-standard-1"
-
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-  }
+  cluster_name = "dev"
 }
 
 # staging environment
 
-# Note: KeyRings cannot be deleted from Google Cloud Platform.
-# Destroying a Terraform-managed KeyRing will remove it from state
-# but will not delete the resource on the server.
-# https://www.terraform.io/docs/providers/google/r/kms_key_ring.html
-resource "google_kms_key_ring" "kms_keyring_staging" {
-  name     = "staging"
-  location = "europe-west1"
-}
+module "staging_environment" {
+  source = "./environments"
 
-resource "google_kms_crypto_key" "kms_crypto_key_etcd_database_password_staging" {
-  name     = "etcd-database-password"
-  key_ring = "${google_kms_key_ring.kms_keyring_staging.self_link}"
-}
-
-resource "google_compute_network" "staging_vpc_network" {
-  name                    = "staging"
-  auto_create_subnetworks = "true"
-}
-
-data "google_container_engine_versions" "container_engine_versions_staging" {
-  version_prefix = "1.13."
-}
-
-resource "google_container_cluster" "staging_container_cluster" {
-  provider = "google-beta"
-
-  name     = "staging"
-  location = "europe-west1"
-  min_master_version = "${data.google_container_engine_versions.container_engine_versions_staging.latest_master_version}"
-
-  network = "${google_compute_network.staging_vpc_network.self_link}"
-
-  database_encryption {
-    state    = "ENCRYPTED"
-    key_name = "${google_kms_crypto_key.kms_crypto_key_etcd_database_password_staging.self_link}"
-  }
-
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
-  remove_default_node_pool = true
-  initial_node_count       = 1
-
-  master_auth {
-    username = ""
-    password = ""
-
-    client_certificate_config {
-      issue_client_certificate = false
-    }
-  }
-}
-
-resource "google_container_node_pool" "staging_container_node_pool" {
-  name       = "staging-node-pool"
-  location   = "europe-west1"
-  cluster    = "${google_container_cluster.staging_container_cluster.name}"
-  node_count = 2
-
-  version = "${data.google_container_engine_versions.container_engine_versions_staging.latest_master_version}"
-
-  node_config {
-    machine_type = "n1-standard-1"
-
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-  }
+  cluster_name = "staging"
 }
